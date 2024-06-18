98722931d3238d689f1969b5fed5bcc8d4e626c7
vickyjha23 <vickyjha388@gmail.com>
2024-06-18 21:20:27 +0530

    news portal project initiated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+       return(
+            <nav className="">
+                 <div>
+                      <div>
+                            <h1><span>News</span> <span>portal</span></h1>
+                      </div>
+                 </div>
+            </nav>
+       )
+}
+
+export default NavBar;
